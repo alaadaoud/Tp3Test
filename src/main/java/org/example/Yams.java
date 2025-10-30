08033966b9fe683e2b9e397a85c9c492d377ed79
Ala Daoud <alaa.daoud.1212@gmail.com>
2025-10-30 10:31:45 +0100

    On vérifie l’ordre : on teste d’abord le carré, puis le brelan, car si un lancer est à la fois carré et brelan, on prend le plus haut score

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -21,7 +21,7 @@ public class Yams {
     }
 
 
-
+    // pour cette fonction lordre de test est important,on teste d abord le carré  puis le brelan ar si un lancer est à la fois carré et brelan, on prend le plus haut score
     public static int CalculerPoints(int[] lancer) {
         if (estCarre(lancer)) return 35;
         if (estBrelan(lancer)) return 28;
