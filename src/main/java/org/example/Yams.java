083107595b66bbce81f4092549802b9ece994df7
Ala Daoud <alaa.daoud.1212@gmail.com>
2025-10-30 10:38:15 +0100

    Les tests passent maintenant pour le full psq jai Implémenter la détection du full lordre est tre important dans la fonction calculerpionts

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -20,12 +20,26 @@ public class Yams {
         return false;
     }
 
+    private static boolean estFull(int[] des) {
+        int[] counts = new int[7];
+        boolean paire = false;
+        boolean brelan = false;
 
+        for (int d : des) counts[d]++;
+
+        for (int count : counts) {
+            if (count == 2) paire = true;
+            if (count == 3) brelan = true;
+        }
+
+        return paire && brelan;
+    }
 
 
 
     public static int CalculerPoints(int[] lancer) {
         if (estCarre(lancer)) return 35;
+        if (estFull(lancer)) return 30;
         if (estBrelan(lancer)) return 28;
         return 0;
     }
