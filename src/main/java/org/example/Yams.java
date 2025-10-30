e89560d076dc728eacf8d137c52320a598e1758c
Ala Daoud <alaa.daoud.1212@gmail.com>
2025-10-30 10:27:28 +0100

    Les tests passent maintenant pour le carre psq jai Implémenter la détection du carre

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -11,9 +11,19 @@ public class Yams {
         return false;
     }
 
+    private static boolean estCarre(int[] des) {
+        int[] counts = new int[7];
+        for (int d : des) {
+            counts[d]++;
+            if (counts[d] >= 4) return true;
+        }
+        return false;
+    }
+
 
 
     public static int CalculerPoints(int[] lancer) {
+        if (estCarre(lancer)) return 35;
         if (estBrelan(lancer)) return 28;
         return 0;
     }
