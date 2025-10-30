9002bb49e60eb5cecb7dced553519616aecedb5d
Ala Daoud <alaa.daoud.1212@gmail.com>
2025-10-30 10:22:10 +0100

    Les tests passent maintenant pour le brelan psq jai Implémenter la détection du Brelan

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public class Yams {
+
+    private static boolean estBrelan(int[] des) {
+        int[] counts = new int[7];
+        for (int d : des) {
+            counts[d]++;
+            if (counts[d] >= 3) return true;
+        }
+        return false;
+    }
+
+
+
     public static int CalculerPoints(int[] lancer) {
+        if (estBrelan(lancer)) return 28;
         return 0;
     }
 }
